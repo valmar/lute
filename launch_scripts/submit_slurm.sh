385af7df0783cd84f5fb9df730b5f9ca033fecd9
gadorlhiac <gadorlhiac@gmail.com>
2024-03-19 14:32:18 -0700

    BUG Fix environment variables in SLURM script

--- launch_scripts/submit_slurm.sh
+++ launch_scripts/submit_slurm.sh
@@ -61,8 +61,8 @@ fi
 SLURM_ARGS=$@
 
 # Setup logfile names - $EXPERIMENT and $RUN will be available if ARP submitted
-FORMAT_RUN=$(printf "%04d" ${RUN_NUM:-0})
-LOG_FILE="${TASK}_${EXPERIMENT:-EXP}_r${FORMAT_RUN}_$(date +'%Y-%m-%d_%H-%M-%S')"
+FORMAT_RUN=$(printf "%04d" ${RUN:-0})
+LOG_FILE="${TASK}_${EXPERIMENT:-$EXP}_r${FORMAT_RUN}_$(date +'%Y-%m-%d_%H-%M-%S')"
 SLURM_ARGS+=" --output=${LOG_FILE}.out"
 SLURM_ARGS+=" --error=${LOG_FILE}.err"
 
